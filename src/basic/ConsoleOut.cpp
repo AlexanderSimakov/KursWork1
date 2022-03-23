3536091cbc5d1681e15a4efa46d719d460ef5798
Alexander <casha4367@mail.ru>
2022-03-23 19:41:17 +0300

    Copy color out functions from helpFunctions file

--- src/basic/ConsoleOut.cpp
+++ src/basic/ConsoleOut.cpp
@@ -45,3 +45,37 @@ void ConsoleOut::clear(){
   		tcsetattr(0, TCSANOW, &old);
 	}
 #endif
+
+void ConsoleOut::show_error(string message, string pref_line, string post_line) {
+	set_color(cons::Color::Red);
+	cout << pref_line + "<-- " + message + " -->" + post_line;
+	set_default_color();
+}
+
+void ConsoleOut::show_title(string title, string pref_line, string post_line) {
+	set_color(cons::Color::Yellow);
+	cout << pref_line + "<-- " + title + " -->" + post_line;
+	set_default_color();
+}
+
+void ConsoleOut::show_info(string info, string pref_line, string post_line) {
+	set_color(cons::Color::Green);
+	cout << pref_line + "<-- " + info + " -->" + post_line;
+	set_default_color();
+}
+
+void ConsoleOut::set_color(cons::Color color) {
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((30 << 4) | color));
+#elif __linux__
+	cout << "\033[" << color << "m";
+#endif
+}
+
+void ConsoleOut::set_default_color(){
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((Color::Black << 4) | Color::White));
+#elif __linux__
+	cout << "\033[0m";
+#endif
+}
--- src/basic/ConsoleOut.h
+++ src/basic/ConsoleOut.h
@@ -13,6 +13,30 @@ using namespace std;
 static struct termios old, current;
 #endif
 
+namespace cons {
+	enum Color {
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+ 		Black = 0,
+		Blue = 1,
+		Green = 2,
+		Cyan = 3,
+		Red = 4,
+		Magenta = 5,
+		Yellow = 14,
+		White = 15  
+#elif __linux__
+		Black = 30,
+		Red = 31,
+		Green = 32,
+		Yellow = 33,
+		Blue = 34,
+		Magenta = 35,
+		Cyan = 36,
+		White = 37,
+#endif
+	};
+}
+
 class ConsoleOut
 {
 private:
@@ -27,4 +51,10 @@ public:
     static void pause();
     static char getchar();
     static void clear();
+    
+    static void show_error(string message, string pref_line = "\t", string post_line = "\n");
+	static void show_title(string title, string pref_line = "\t", string post_line = "\n");
+	static void show_info(string info, string pref_line = "\t", string post_line = "\n");
+	static void set_color(cons::Color color);
+	static void set_default_color();
 };
