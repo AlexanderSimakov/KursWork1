b7bb9a5f7d97b8a24b7df7616fbb9bb4f8461042
Alexander <casha4367@mail.ru>
2022-03-23 19:14:19 +0300

    Add getchar and pause functions

--- src/basic/ConsoleOut.cpp
+++ src/basic/ConsoleOut.cpp
@@ -2,9 +2,38 @@
 #include "ConsoleOut.h"
 
 void ConsoleOut::pause(){
-
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+    system("pause");
+#elif __linux__
+    cout << "Press any key to continue..." << endl;
+    ConsoleOut::getchar();
+#endif
 }
 
-void ConsoleOut::getch(){
-
+char ConsoleOut::getchar()
+{	
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+	return _getch();
+#elif __linux__
+	initTermios();
+	char ch = std::getchar();
+	resetTermios();
+	return ch;
+#endif
 }
+
+#if __linux__
+	void ConsoleOut::initTermios() 
+	{
+  		tcgetattr(0, &old);
+  		current = old;
+  		current.c_lflag &= ~ICANON;
+    	current.c_lflag &= ~ECHO;
+  		tcsetattr(0, TCSANOW, &current);
+	}
+
+	void ConsoleOut::resetTermios(void) 
+	{
+  		tcsetattr(0, TCSANOW, &old);
+	}
+#endif
--- src/basic/ConsoleOut.h
+++ src/basic/ConsoleOut.h
@@ -2,15 +2,28 @@
 #include <string>
 #include <iostream>
 
+#if __linux__
+#include <termios.h>
+#include <stdio.h>
+#endif
+
+using namespace std;
+
+#if __linux__
+static struct termios old, current;
+#endif
+
 class ConsoleOut
 {
 private:
 
+#if __linux__
+	static void initTermios();
+	static void resetTermios(void);
+	
+#endif
+
 public:
     static void pause();
-    static void getch();
+    static char getchar();
 };
-
-
-
-
