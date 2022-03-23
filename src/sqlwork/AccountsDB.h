644648db4b3af9fefd3ae1b483f350232bacac50
Alexander <casha4367@mail.ru>
2022-03-23 21:13:06 +0300

    Add functions

--- src/sqlwork/AccountsDB.h
+++ src/sqlwork/AccountsDB.h
@@ -1,17 +1,28 @@
 #pragma once
 #include "SQLWORK.h"
 #include "../basic/ConsoleOut.h"
+#include "../basic/account.h"
+#include "../basic/helpFunctions.h"
 #include <string>
 
 using namespace std;
 
 class AccountsDB
 {
-private:
-	static SQLWork *data_base;
-
 public:
-    static const string DATABASE_FILENAME;
+    void init();
+    void show_table();
+    void add_new(Account account); 
+    void _delete(string login);
+    void confirm(string login);
+    void block(string login);
+    void update_login(string old_login, string new_login);
+    void update_password(string login, string password);
+    void update_role(string login, int role);
+
+    
+private:
+	static const string DATABASE_FILENAME;
     static const string DATABASE_NAME;
     static const string LOGIN;
     static const string HASH;
@@ -19,9 +30,7 @@ public:
     static const string ROLE;
     static const string ACCESS;
 
-
-    
-
+    static SQLWork data_base;
 };
 
 
