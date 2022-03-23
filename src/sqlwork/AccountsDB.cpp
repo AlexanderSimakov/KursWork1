f6a2c388692d192936ebef854f3bada8eb6ee47b
Alexander <casha4367@mail.ru>
2022-03-23 20:25:36 +0300

    Add constatns

--- src/sqlwork/AccountsDB.cpp
+++ src/sqlwork/AccountsDB.cpp
@@ -1,2 +1,11 @@
 #include "AccountsDB.h"
 
+const string AccountsDB::DATABASE_FILENAME = "accounts.db";
+const string AccountsDB::DATABASE_NAME = "ACCOUNTS";
+const string AccountsDB::LOGIN = "LOGIN";
+const string AccountsDB::HASH = "HASH";
+const string AccountsDB::SALT = "SALT";
+const string AccountsDB::ROLE = "ROLE";
+const string AccountsDB::ACCESS = "ACCESS";
+
+
--- src/sqlwork/AccountsDB.h
+++ src/sqlwork/AccountsDB.h
@@ -1,10 +1,26 @@
 #pragma once
+#include "SQLWORK.h"
+#include "../basic/ConsoleOut.h"
+#include <string>
+
+using namespace std;
 
 class AccountsDB
 {
 private:
+	static SQLWork *data_base;
 
 public:
+    static const string DATABASE_FILENAME;
+    static const string DATABASE_NAME;
+    static const string LOGIN;
+    static const string HASH;
+    static const string SALT;
+    static const string ROLE;
+    static const string ACCESS;
+
+
+    
 
 };
 
