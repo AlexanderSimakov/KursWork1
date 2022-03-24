a21a895d4bdc845d1f7f54400a31dfa3b3eedb49
Alexander <casha4367@mail.ru>
2022-03-24 15:24:44 +0300

    Fill Product class

--- src/basic/Product.cpp
+++ src/basic/Product.cpp
@@ -1,2 +1,62 @@
 #include "Product.h"
 
+Product::Product(){ }
+
+Product::Product(string name, int amount, int price, string date, string registrant)
+{
+    this->name = name;
+    this->amount = amount;
+    this->price = price;
+    this->date = date;
+    this->registrant = registrant; 
+}
+
+void Product::set_name(string name)
+{
+    this->name = name;
+}
+
+void Product::set_amount(int amount)
+{
+    this->amount = amount;
+}
+
+void Product::set_price(int price)
+{
+    this->price = price;
+}
+
+void Product::set_date(string date)
+{
+    this->date = date;
+}
+
+void Product::set_registrant(string registrant)
+{
+    this->registrant = registrant;
+}
+
+string Product::get_name()
+{
+    return name;
+}
+
+int Product::get_amount()
+{
+    return amount;
+}
+
+int Product::get_price()
+{
+    return price;
+}
+
+string Product::get_date()
+{
+    return date;
+}
+
+string Product::get_registrant()
+{
+    return registrant;
+}
--- src/basic/Product.h
+++ src/basic/Product.h
@@ -1,12 +1,30 @@
 #pragma once
+#include <string>
 
 using namespace std;
 
 class Product
 {
 public:
+    Product();
+    Product(string name, int amount, int price, string date, string registrant);
+
+    void set_name(string name);
+    void set_amount(int amount);
+    void set_price(int price);
+    void set_date(string date);
+    void set_registrant(string registrant);
+
+    string get_name();
+    int get_amount();
+    int get_price();
+    string get_date();
+    string get_registrant();
 
 private:
-
+    string name;
+    int amount;
+    int price;
+    string date;
+    string registrant;
 };
-
