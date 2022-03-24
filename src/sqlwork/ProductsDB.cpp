3089d8a6f9c0ca725be16e6c7afd4518248927df
Alexander <casha4367@mail.ru>
2022-03-24 13:34:45 +0300

    Fill ProductsDB class

--- src/sqlwork/ProductsDB.cpp
+++ src/sqlwork/ProductsDB.cpp
@@ -1,2 +1,123 @@
 #include "ProductsDB.h"
 
+static const string DATABASE_FILENAME;
+static const string DATABASE_NAME;
+static const string NAME = "NAME";
+static const string AMOUNT = "AMOUNT";
+static const string PRICE = "PRICE";
+static const string DATE = "DATE";
+static const string REGISTRANT = "REG_NAME";
+
+
+ProductsDB::ProductsDB() { }
+
+ProductsDB::ProductsDB(SQLWork *db){ this->db = db; }
+
+void ProductsDB::init()
+{
+    db = new SQLWork(DATABASE_FILENAME);
+
+    db->open();
+	db->create_table_if_not_exists({ SQL_cell{ NAME,     "TEXT PRIMARY KEY NOT NULL"},
+				SQL_cell{ AMOUNT,   "INT NOT NULL"},
+				SQL_cell{ PRICE,    "INT NOT NULL"},
+				SQL_cell{ DATE,     "TEXT NOT NULL"},
+				SQL_cell{ REGISTRANT, "TEXT NOT NULL"} },
+		        DATABASE_NAME);
+}
+
+void ProductsDB::show_table(string sql_end)
+{
+	db->show_table("SELECT * FROM ", " " + sql_end,
+		{ "         name", "amount", "  price", "date", "    name of registrant" },
+		{ 0, 1, 2, 3, 4 },
+		{ 28, 13, 12, 19, 35 });
+}
+
+void ProductsDB::show_table()
+{
+    ConsoleOut::show_title("products", "                                          ");
+    show_table(";");
+	cout << endl;
+}
+
+void ProductsDB::add_new(Product product)
+{
+    db->push_back({ "'" + product.name + "'",
+		to_string(product.amount),
+		"'" + to_string(product.price) + "'",
+		"'" + product.date + "'",
+		"'" + product.name_of_registrant + "'" });
+}
+
+void ProductsDB::_delete(string name)
+{
+	db->delete_field(NAME + "='" + name + "'");
+}
+
+void ProductsDB::update_name(string name, string new_name)
+{
+	db->update(NAME, "'" + new_name + "'", NAME + "='" + name + "'");
+}
+
+void ProductsDB::update_amount(string name, int amount)
+{
+	db->update(AMOUNT, "'" + to_string(amount) + "'", NAME + "='" + name + "'");
+}
+
+void ProductsDB::update_price(string name, int price)
+{
+	db->update(PRICE, "'" + to_string(price) + "'", NAME + "='" + name + "'");
+}
+
+void ProductsDB::update_date(string name, string date)
+{
+	db->update(DATE, "'" + date + "'", NAME + "='" + name + "'");
+}
+
+void ProductsDB::update_registrant(string name, string registrant)
+{
+	db->update(REGISTRANT, "'" + registrant + "'", NAME + "='" + name + "'");
+}
+
+void ProductsDB::find_by_name(string name)
+{
+    show_table("WHERE " + NAME + " GLOB '*" + name + "*';");
+	ConsoleOut::pause();
+}
+
+void ProductsDB::find_by_registrant(string registrant)
+{
+    show_table("WHERE " + REGISTRANT + " GLOB '*" + registrant + "*';");
+	ConsoleOut::pause();
+}
+
+void ProductsDB::find_by_date(string date)
+{
+    show_table("WHERE " + DATE + " GLOB '" + date + "';");
+	ConsoleOut::pause();
+}
+    
+void ProductsDB::show_sorted_by_name()
+{
+    ConsoleOut::show_title("Products sorted by name", "\t", "\n\n");
+	show_table("ORDER BY " + NAME + " ASC ;");
+    cout << endl;
+	ConsoleOut::pause();
+}
+
+void ProductsDB::show_sorted_by_price_to_higher()
+{
+    ConsoleOut::show_title("Products sorted by price (to higher)", "\t", "\n\n");
+	show_table("ORDER BY " + PRICE + " ASC ;");
+    cout << endl;
+	ConsoleOut::pause();
+}
+
+void ProductsDB::show_sorted_by_amount_to_higher()
+{
+    ConsoleOut::show_title("products sorted by amount (to higher)", "\t", "\n\n");
+	show_table("ORDER BY " + AMOUNT + " ASC ;");
+    cout << endl;
+	ConsoleOut::pause();
+}
--- src/sqlwork/ProductsDB.h
+++ src/sqlwork/ProductsDB.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "SQLWORK.h"
 #include <string>
+#include "../basic/ConsoleOut.h"
+#include "../basic/product.h"
 
 using namespace std;
 
@@ -12,21 +14,23 @@ public:
     void init();
 
     void show_table();
-    void add_new();
-    void _delete();
-    void update_name();
-    void update_amount();
-    void update_price();
-    void update_date();
-    void update_registrant();
+    void add_new(Product product);
+    void _delete(string name);
+    void update_name(string name, string new_name);
+    void update_amount(string name, int amount);
+    void update_price(string name, int price);
+    void update_date(string name, string date);
+    void update_registrant(string name, string registrant);
 
-    void find_by_name();
-    void find_by_registrant();
-    void find_by_date();
+    void find_by_name(string name);
+    void find_by_registrant(string registrant);
+    void find_by_date(string date);
     
     void show_sorted_by_name();
     void show_sorted_by_price_to_higher();
     void show_sorted_by_amount_to_higher();
+
+    SQLWork *db;
 
 private:
     static const string DATABASE_FILENAME;
@@ -36,4 +40,6 @@ private:
     static const string PRICE;
     static const string DATE;
     static const string REGISTRANT;
+
+    void show_table(string sql_end);
 };
