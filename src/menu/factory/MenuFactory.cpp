1eba247e12d7ff47c8378fac8866154320121933
Alexander <casha4367@mail.ru>
2022-03-27 00:22:48 +0300

    Fill MenuFactory class

--- src/menu/factory/MenuFactory.cpp
+++ src/menu/factory/MenuFactory.cpp
@@ -1,2 +1,60 @@
 #include "MenuFactory.h"
 
+Menu* MenuFactory::create_admin_menu(){
+    Menu* menu = new Menu("<- Admin menu ->",
+		{ " Accounts",
+		  " Products",
+		  " Log Out" });
+	menu->set_start_with_first_line(true);
+    return menu;
+}
+
+Menu* MenuFactory::create_accounts_manage_menu(){
+    return new Menu("<- Accounts ->",
+		{ " Show",
+		  " Add new",
+		  " Edit",
+		  " Delete",
+		  " Confirm",
+		  " Block",
+		  " Exit" }); 
+}
+
+Menu* MenuFactory::create_products_manage_menu(){
+    return new Menu("<- Products ->",
+		{ " Show",
+		  " Add",
+		  " Delete",
+		  " Edit",
+		  " Find by name",
+		  " Find by name of registrant",
+		  " Find by date",
+		  " Sort by name",
+		  " Sort by price (to higher)",
+		  " Sort by amount (to higher)",
+		  " Exit" });
+}
+
+Menu* MenuFactory::create_user_menu(){
+    return new Menu("<- Products ->",
+		{ " Show",
+		  " Find by name",
+		  " Find by registrant name",
+		  " Find by date",
+		  " Sort by name",
+		  " Sort by price (to higher)",
+		  " Sort by amount (to higher)",
+		  " Log out" });
+}
+
+Menu* MenuFactory::create_account_edit_menu(){
+    Menu* menu = new Menu({ " login", " password", " role", " exit" });
+	menu->set_start_with_first_line(true);
+    return menu;
+}
+
+Menu* MenuFactory::create_product_edit_menu(){
+    Menu* menu = new Menu({ " Name", " Amount", " Price", " Date", " Registrant", " Exit" });
+	menu->set_start_with_first_line(true);
+    return menu;
+}
--- src/menu/factory/MenuFactory.h
+++ src/menu/factory/MenuFactory.h
@@ -1,9 +1,14 @@
 #pragma once
+#include <string>
 #include "../menu.h"
 
 class MenuFactory
 {
 public:
-
-
+    static Menu* create_admin_menu();
+    static Menu* create_accounts_manage_menu();
+    static Menu* create_products_manage_menu();
+    static Menu* create_user_menu();
+    static Menu* create_account_edit_menu();
+    static Menu* create_product_edit_menu();
 };
