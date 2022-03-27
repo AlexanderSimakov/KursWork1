7cc0373d435d35561000f315675a9bc3eed97d03
Alexander <casha4367@mail.ru>
2022-03-27 13:21:09 +0300

    Fill AdminSession class

--- src/manage/AdminSession.cpp
+++ src/manage/AdminSession.cpp
@@ -1,2 +1,19 @@
 #include "AdminSession.h"
 
+AdminSession::AdminSession(AccountsDB* accountsDB, ProductsDB* productsDB){
+    this->productsdb = productsDB;
+	this->accountsDB = accountsDB;
+    {
+    	admin_menu = MenuFactory::create_admin_menu();
+	    accounts_manage_menu = MenuFactory::create_accounts_manage_menu();
+    	products_manage_menu = MenuFactory::create_products_manage_menu();
+	    account_edit_menu = MenuFactory::create_account_edit_menu();
+	    product_edit_menu = MenuFactory::create_product_edit_menu();
+	    user_menu = MenuFactory::create_user_menu();
+    }
+}
+
+void AdminSession::start(Account* account){
+    session_account = account;
+     
+}
--- src/manage/AdminSession.h
+++ src/manage/AdminSession.h
@@ -1,10 +1,43 @@
 #pragma once
-
-
-class AdminSession
+#include "session.cpp"
+#include "../menu/menu.h"
+#include "../menu/confirm/ConfirmationMenu.h"
+#include "../menu/factory/MenuFactory.h"
+#include "../sqlwork/AccountsDB/AccountsDB.h"
+#include "../sqlwork/ProductsDB/ProductsDB.h"
+class AdminSession : Session
 {
-private:
-
 public:
+    AdminSession(AccountsDB* accountsDb, ProductsDB* productsDB);
+    void start(Account* account);
 
+private:
+    AccountsDB* accountsDB = nullptr;
+    Menu *admin_menu = nullptr;
+	Menu *accounts_manage_menu = nullptr;
+	Menu *products_manage_menu = nullptr;
+	Menu *user_menu = nullptr;
+	Menu *account_edit_menu = nullptr;
+	Menu *product_edit_menu = nullptr;
+
+    void start_accounts_managing();
+	void start_products_managing();
+
+	void add_new_account(); 
+	void delete_account();
+	void confirm_account();
+	void block_account();
+	void start_account_editing(); 
+	void edit_account_login(string *login);
+	void edit_account_password(string login);
+	void edit_account_role(string login);
+
+	void add_new_product();
+	void delete_product();
+	void start_product_editing(); 
+	void edit_product_name(string *name); 
+	void edit_product_amount(string name);
+	void edit_product_price(string name);
+	void edit_product_date(string name); 
+	void edit_product_registrant(string name);
 };
