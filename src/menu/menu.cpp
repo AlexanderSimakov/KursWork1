d27b00235f4e9f75615efa1d53424df1ac7775c3
Alexander <casha4367@mail.ru>
2022-03-29 14:43:46 +0300

    Refactor Menu class

--- src/menu/menu.cpp
+++ src/menu/menu.cpp
@@ -1,108 +1,123 @@
 #pragma once
 #include "menu.h"
 
-Menu::Menu() {
+Menu::Menu() 
+{
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 	std_handle = GetStdHandle(STD_OUTPUT_HANDLE);
 #endif
 }
 
-Menu::Menu(vector<string> lines) {
+Menu::Menu(vector<string> lines) 
+{
 	Menu();
 	this->lines = lines;
 }
 
-Menu::Menu(string title, vector<string> lines) {
+Menu::Menu(string title, vector<string> lines) 
+{
 	Menu();
 	this->title = title;
 	this->lines = lines;
 }
 
-int Menu::get_num_of_choisen_line() {
-	char pressed_key = 0;
+int Menu::get_num_of_choisen_line() 
+{
 	clear();
 	print_title();
 	print_lines();
 
-	if (is_start_with_first_line) {
-		num_of_choisen_line = 0;
-	}
+	if (is_start_with_first_line) num_of_choisen_line = 0;
 
-	while (true) {
+	int pressed_key = 0;
+	while (true) 
+	{
 		print_pointer();
-		pressed_key = ConsoleOut::getchar();
+		pressed_key = (int)ConsoleOut::getchar();
 
-		if ((int)pressed_key == Buttons::ENTER) {
+		if (pressed_key == Buttons::ENTER) 
+		{
 			clear();
 			return num_of_choisen_line;
 		}
-		else if ((int)pressed_key == Buttons::KEU_UP && num_of_choisen_line > 0) {
+		else if (pressed_key == Buttons::KEU_UP && num_of_choisen_line > 0) 
+		{
 			num_of_choisen_line--;
 		}
-		else if ((int)pressed_key == Buttons::KEY_DOWN && num_of_choisen_line < lines.size() - 1) {
+		else if (pressed_key == Buttons::KEY_DOWN && num_of_choisen_line < lines.size() - 1) 
+		{
 			num_of_choisen_line++;
 		}
-		else if ((int)pressed_key == Buttons::ESC) {
+		else if (pressed_key == Buttons::ESC) 
+		{
 			clear();
 			return -1;
 		}
 	}
 }
 
-void Menu::print_title() {
-	if (title.size()) cout << title << std::endl;
+void Menu::print_title() 
+{
+	if (title.size()) cout << title << endl;
 }
 
-void Menu::print_lines() {
-	for (int i = 0; i < lines.size(); i++) {
-		cout << " " << lines[i] << endl;
-	}
+void Menu::print_lines() 
+{
+	for (int i = 0; i < lines.size(); i++)  cout << " " << lines[i] << endl;
 }
 
-void Menu::print_pointer() {
+void Menu::print_pointer() 
+{
 	int add = 0;
 	if (title.size()) add = 1;
-	for (int i = 0; i < lines.size(); i++) {
-		if (num_of_choisen_line == i) {
+	for (int i = 0; i < lines.size(); i++) 
+	{
+		if (num_of_choisen_line == i) 
+		{
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 			FillConsoleOutputCharacter(std_handle, (TCHAR)'>', 1, { 0, (SHORT)(i + add) }, &cWrittenChars);
 		else{
 			FillConsoleOutputCharacter(std_handle, (TCHAR)' ', 1, { 0, (SHORT)(i + add) }, &cWrittenChars);
 		}
-
 #elif __linux__
 			printf("\033[%d;%dH>", i + add + 1, 0);
-			printf("\033[%d;%dH", 10, 30);
+			printf("\033[%d;%dH", 20, 30);
 		}
 		else {
 			printf("\033[%d;%dH ", i + add + 1, 0);
-			printf("\033[%d;%dH", 10, 30);
+			printf("\033[%d;%dH", 20, 30);
 		}
 #endif
 	}
 }
 
-void Menu::add_line(string line) {
+void Menu::add_line(string line) 
+{
 	lines.push_back(line);
 }
 
-void Menu::set_lines(vector<string> lines) {
+void Menu::set_lines(vector<string> lines) 
+{
 	this->lines = lines;
 }
 
-void Menu::set_title(string title) {
+void Menu::set_title(string title) 
+{
 	this->title = title;
 }
 
-void Menu::set_start_with_first_line(bool is_start_with_first_line) {
+void Menu::set_start_with_first_line(bool is_start_with_first_line) 
+{
 	this->is_start_with_first_line = is_start_with_first_line;
 }
 
-void Menu::set_pointer_to_start() {
+void Menu::set_pointer_to_start() 
+{
 	num_of_choisen_line = 0;
 }
 
-void Menu::clear(){
+void Menu::clear()
+{
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 	system("cls");
 #elif __linux__
